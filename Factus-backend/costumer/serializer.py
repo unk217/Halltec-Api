0667346548ea8660854254cdebbee96945b9619b
unk217 <unk00@hotmail.com>
2025-01-29 16:26:58 -0500

    update structure of response django

--- Factus-backend/costumer/serializer.py
+++ Factus-backend/costumer/serializer.py
@@ -2,8 +2,11 @@ from rest_framework import serializers
 from .models import Costumer
 
 class CostumerSerializer(serializers.ModelSerializer):
-     tipo_documento_display = serializers.ReadOnlyField(source='DOCUMENT_TYPE_ID', read_only=True)
+     #tipo_documento_display = serializers.ReadOnlyField(source='DOCUMENT_TYPE_ID', read_only=True)
     
      class Meta:
         model = Costumer
-        fields = ['id', 'document_type', 'document_number', 'company', 'names', 'address', 'email', 'phone', 'p_type', 'tribute', 'municipal', 'tipo_documento_display']
+        fields = ['id', 'document_type', 'document_number', 'company',
+                  'names', 'address', 'email',
+                  'phone', 'p_type', 'tribute', 
+                  'municipal', ]
--- Factus-backend/costumer/views.py
+++ Factus-backend/costumer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import viewsets
-
 from .serializer import CostumerSerializer
 from .models import Costumer
 
@@ -8,4 +8,20 @@ from .models import Costumer
 class CostumerView(viewsets.ModelViewSet):
     serializer_class = CostumerSerializer
     queryset = Costumer.objects.all()
+    
+    
+    def list(self, request, *args, **kwargs):
+        costumers = self.get_queryset()
+        serializer = self.get_serializer(costumers, many=True)
+        
+        response_data = {
+            'choices': {
+                'document_type': dict(Costumer.DOCUMENT_TYPE_ID),
+                'p_type': dict(Costumer.PERSON_TYPE),
+                'tax_type': dict(Costumer.TAX_TYPE),
+            },
+            'costumers': serializer.data,
+            
+        }
+        return Response(response_data)
     
