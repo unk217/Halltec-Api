e4458bbc84a98d9ac726e72bb37301be9c6e92b3
unk217 <unk00@hotmail.com>
2025-01-28 19:38:45 -0500

    add item on serializer for choicesId

--- Factus-backend/costumer/serializer.py
+++ Factus-backend/costumer/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Costumer
 
 class CostumerSerializer(serializers.ModelSerializer):
-    class Meta:
+     tipo_documento_display = serializers.ReadOnlyField(source='DOCUMENT_TYPE_ID', read_only=True)
+    
+     class Meta:
         model = Costumer
-        fields = '__all__'
+        fields = ['id', 'document_type', 'document_number', 'company', 'names', 'address', 'email', 'phone', 'p_type', 'tribute', 'municipal', 'tipo_documento_display']
