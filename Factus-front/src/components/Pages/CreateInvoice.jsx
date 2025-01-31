30b64f54f0baea0189b254b4021d62f4230e01bc
unk217 <unk00@hotmail.com>
2025-01-30 21:04:16 -0500

    add fields in create invoices for costumer

--- Factus-front/src/components/Pages/CreateInvoice.jsx
+++ Factus-front/src/components/Pages/CreateInvoice.jsx
@@ -8,8 +8,10 @@ import Select from "react-select";
 function CreateInvoice() {
   const [idcos, setIdcos] = useState([]);
   const [idchoices, setIdchoices] = useState([]);
+  const [tribute, setTribute] = useState([]);
+  const [taxType, setTaxType] = useState([]);
   const [mun, setMun] = useState([]);
-  /*const {
+  const {
     register,
     handleSubmit,
     formState: { errors },
@@ -23,27 +25,33 @@ function CreateInvoice() {
     } catch (error) {
       console.log("error fetching data", error);
     }
-  });*/
+  });
 
   useEffect(() => {
     const loadCostumers = async () => {
       try {
         const apiUrl = "http://127.0.0.1:8000/costumer/api/v1/costumer/";
         const res = await axios.get(apiUrl);
-        console.log(res)
-        {/*const idcos = res.data.map((ids) => ({
+        console.log(res);
+        {
+          /*const idcos = res.data.map((ids) => ({
           name: ids.names,
           //idChoice: ids.tipo_documento_display,
         }));
         setIdcos(idcos);
-        */}
-        
-        const idchoices = Object.entries(res.data.choices.document_type || {}).map(
-          ([value, label])=>({value, label}))
+        */
+        }
 
-        setIdchoices(idchoices)
+        const idchoices = res.data.choices.document_type;
+        setIdchoices(idchoices);
         console.log(res);
         console.log(idchoices);
+
+        const tribute = res.data.choices.p_type;
+        setTribute(tribute);
+
+        const taxType = res.data.choices.tax_type;
+        setTaxType(taxType);
       } catch (error) {
         console.log("error en costumer", error);
       }
@@ -65,12 +73,12 @@ function CreateInvoice() {
           },
         });
 
-        const mun = res.data.data.map((m)=>({
+        const mun = res.data.data.map((m) => ({
           code: m.code,
           department: m.department,
           id: m.id,
           name: m.name,
-        }))
+        }));
 
         setMun(mun);
         //console.log(res);
@@ -96,81 +104,84 @@ function CreateInvoice() {
       <h1 className="text-white font-extrabold flex justify-center text-6xl p-4">
         CREAR FACTURA
       </h1>
-      <label className="text-white font-bold py-4">Tipo de documento</label>
-      <Select
-        defaultValue={idchoices[1]}
-        options={idchoices}
-        onChange={handleSelectChange}
-      />
-      <label className="text-white font-bold py-4">Municipios</label>
-      <Select
-        defaultValue={idchoices[1]}
-        options={mun.map((i)=>({label: i.name, value: i.id}))}
-        onChange={handleSelectChange}
-      />
-      {/* <div className="columns-2 ">
-        <form onSubmit={onSubmit}>
-          <Input
-            type="text"
-            placeholder="Tipo de identificación"
-            {...register("tId", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="id"
-            {...register("id", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="empresa"
-            {...register("company", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="nombre"
-            {...register("names", { required: true })}
-          />
-          <Select options={document} />
-
-          <Input
-            type="text"
-            placeholder="direccion"
-            {...register("addrest", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="email"
-            {...register("email", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="celular"
-            {...register("phone", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="tipo de persona"
-            {...register("typeId", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="tributante"
-            {...register("tributeType", { required: true })}
-          />
-          <Input
-            type="text"
-            placeholder="municipio"
-            {...register("municipality", { required: true })}
-          />
-
-          <button
-            className="bg-lime-700 p-3 rounded-lg block w-32 mt-3
+      <div className="flex justify-center">
+        <div className="columns-2">
+          <form onSubmit={onSubmit}>
+            <label className="text-white font-bold py-4">
+              Tipo de documento
+            </label>
+            <Select
+              defaultValue={idchoices[1]}
+              options={idchoices}
+              onChange={handleSelectChange}
+            />
+            <label className="text-white font-bold py-4">
+              Número de documento
+            </label>
+            <Input
+              type="text"
+              placeholder="id"
+              {...register("id", { required: true })}
+            />
+            <label className="text-white font-bold py-4">Tipo de persona</label>
+            <Select
+              defaultValue={idchoices[1]}
+              options={tribute}
+              onChange={handleSelectChange}
+            />
+            <label className="text-white font-bold py-4">
+              Tipo de impuesto
+            </label>
+            <Select
+              defaultValue={idchoices[1]}
+              options={taxType}
+              onChange={handleSelectChange}
+            />
+            <label className="text-white font-bold py-4">Municipios</label>
+            <Select
+              defaultValue={idchoices[2]}
+              options={mun.map((i) => ({ label: i.name, value: i.id }))}
+              onChange={handleSelectChange}
+            />
+            <label className="text-white font-bold py-4">Empresa</label>
+            <Input
+              type="text"
+              placeholder="empresa"
+              {...register("company", { required: true })}
+            />
+            <label className="text-white font-bold py-4">Nombres</label>
+            <Input
+              type="text"
+              placeholder="nombre"
+              {...register("names", { required: true })}
+            />
+            <label className="text-white font-bold py-4">Dirección</label>
+            <Input
+              type="text"
+              placeholder="direccion"
+              {...register("addrest", { required: true })}
+            />
+            <label className="text-white font-bold py-4">Email</label>
+            <Input
+              type="text"
+              placeholder="email"
+              {...register("email", { required: true })}
+            />
+            <label className="text-white font-bold py-4">Celular</label>
+            <Input
+              type="text"
+              placeholder="celular"
+              {...register("phone", { required: true })}
+            />
+            <button
+              className="bg-lime-700 p-3 rounded-lg block w-32 mt-3
          hover:bg-lime-600 hover:cursor-pointer font-bold uppfont-bold uppercase"
-          >
-            Save
-          </button>
-        </form>
-      </div>*/}
+            >
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
